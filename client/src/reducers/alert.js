5d495f8de888307c579ac91838d60d86b27391a8
Andy Gala <livelifebrands@gmail.com>
2020-07-23 18:12:55 -0700

    Add password alerts

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,9 +1,16 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
+
 export default (state = initialState, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case SET_ALERT:
-      return [...state, action.payload];
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
   }
 };
